aea381af741e0bfb6db7b1cfc6eb549ef667613a
Along <Asir0020@student.monash.edu>
2026-05-22 21:30:36 +1000

    Update 2.1: Add Ball

--- src/main.py
+++ src/main.py
@@ -27,6 +27,14 @@ paddle_2.penup()
 paddle_2.goto(350,0)
 
 #==Ball==
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
+
 
 # Loop game
 while True:
