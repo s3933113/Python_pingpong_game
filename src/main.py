c9cee85180215df06ac7fdeeea1d68e1715bea4a
Along <Asir0020@student.monash.edu>
2026-05-22 20:22:39 +1000

    Update 1.0 : Adding the window screen section

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import turtle
+
+""" window screen section """
+window = turtle.Screen()
+window.title("The Pingpong game")
+window.bgcolor("black")
+window.setup(width=1200, height=1600)
+window.tracer(0)
+
+
