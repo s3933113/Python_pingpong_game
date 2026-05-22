18628d7ea82a24a3aed0b7ded2d2832493aed027
Along <Asir0020@student.monash.edu>
2026-05-22 21:27:19 +1000

    Update 2.0 : Adding the Paddles

--- src/main.py
+++ src/main.py
@@ -1,10 +1,33 @@
 import turtle
 
-""" window screen section """
+#--window screen section--
 window = turtle.Screen()
 window.title("The Pingpong game")
 window.bgcolor("black")
-window.setup(width=1200, height=1600)
+window.setup(width=800, height=600)
 window.tracer(0)
 
 
+# ==Paddle 1==
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("white")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350,0)
+
+# ==Paddle 2==
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350,0)
+
+#==Ball==
+
+# Loop game
+while True:
+    window.update()
